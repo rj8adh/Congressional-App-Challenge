0ce3c57b269ea84b9f383e1fe6608d7ac2336376
namishjoshi <mumalnamish@gmail.com>
2025-10-04 16:45:30 -0500

    Fixed background.js error when blocking websites.

--- background.js
+++ background.js
@@ -2,8 +2,8 @@ var blockedSites = ["youtube.com", "instagram.com", "reddit.com"]
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === "complete" && tab.url) { // tab.url
-        if (blockedSites.some(site => tab.url.includes("youtube.com"))) { // tab.url
-            var redirectUrl = chrome.runtime.getUrl("https://namishj.github.io/match-master/");
+        if (blockedSites.some(site => tab.url.includes(site))) { // tab.url
+            var redirectUrl = chrome.runtime.getURL("YTBlocker.html");
             chrome.tabs.update(tabId, { url: redirectUrl })
             console.log("Blocked", tab.url);
         }
